c465bf716e876cffa273a8a88b8c9aba347a26a0
Claude Lehmann <e.dualc@hotmail.com>
2022-04-19 09:54:08 +0200

    change lcl3 to lcl4

--- run_reconstructed_model.py
+++ run_reconstructed_model.py
@@ -76,10 +76,10 @@ def train_network(config):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--lr', type=float, default=3e-4, help='Learning rate of VGG19\'s optimizer')
-    parser.add_argument('--num_multiplex', type=int, default=4, help='Number of multiplex cells in LCL layers')
+    parser.add_argument('--num_multiplex', type=int, default=3, help='Number of multiplex cells in LCL layers')
     parser.add_argument('--batch_size', type=int, default=10, help='Batch size')
     parser.add_argument('--num_epochs', type=int, default=5, help='Number of epochs trained')
-    parser.add_argument('--lcl_distance', type=int, default=3, help='Size of kernel filters for K, as k=2*d+1 with [k x k] sized filters')
+    parser.add_argument('--lcl_distance', type=int, default=2, help='Size of kernel filters for K, as k=2*d+1 with [k x k] sized filters')
     parser.add_argument('--lcl_alpha', type=float, default=1e-3, help='Rate at which kernel K is changed by K_change')
     parser.add_argument('--lcl_eta', type=float, default=0.01, help='Rate at which the output is changed by O=(1-eta)*A+eta*L')
     parser.add_argument('--lcl_theta', type=float, default=0.2, help='How much the noise is added to the LCL training (breaking symmetry)')
